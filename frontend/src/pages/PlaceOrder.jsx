653c174922a62b9c80501de3d4d34eaa73ad8eee
74sonuydv <yadavsonu34785@gmail.com>
2026-01-16 22:03:17 +0530

    add stripe and razorpay checkout flow

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -34,6 +34,30 @@ const PlaceOrder = () => {
     }));
   }
 
+  const initPAy = (order) => {
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: 'order Payment',
+      description: 'order Payment',
+      receipt: order.receipt,
+      handler: async (response) => {
+        try {
+          const {data} = await axios.post(backendUrl + '/api/order/verifyRazorpay', response, {headers:{token}});
+          if (data.success) {
+            navigate('/orders');
+            setCartItems({});
+          }
+        } catch (error) {
+          toast.error(error.message);
+        }
+      }
+    }
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+  }
+
   const onSubmitHandler = async (event) => {
     event.preventDefault();
     try {
@@ -70,6 +94,23 @@ const PlaceOrder = () => {
             toast.error(response.data.message);
           }
           break;
+        }
+        case 'stripe': {
+          const responseStripe = await axios.post(backendUrl + '/api/order/stripe', orderData, {headers:{token}});
+          if (responseStripe.data.success) {
+            const {session_url} = responseStripe.data;
+            window.location.replace(session_url);
+          }else{
+            toast.error(responseStripe.data.message);
+          }
+        }
+        case 'razorpay': {
+          const responseRazorpay = await axios.post(backendUrl + '/api/order/razorpay', orderData, {headers:{token}});
+          if (responseStripe.data.success) {
+            initPAy(responseRazorpay.data.order);
+          }else{
+            toast.error(responseRazorpay.data.message);
+          }
         }
         default:
           break;
