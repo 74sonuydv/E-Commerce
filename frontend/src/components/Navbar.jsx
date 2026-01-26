f17a2ee39058d6b05466e4de0c5cf23b1d306da4
74sonuydv <yadavsonu34785@gmail.com>
2026-01-26 13:26:39 +0530

    update Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
             </ul>
 
             <div className='flex items-center gap-6'>
-                <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+                <img onClick={() => {setShowSearch(true); navigate('/collection')}} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
                 <div className='group relative'>
                     <img onClick={() => token ? 'null' : navigate('/login')} src={assets.profile_icon} className='w-5 cursor-pointer' alt='' />
